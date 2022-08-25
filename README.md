f7a244e9b7904398c637a7f96a728ed33dd42020
Agustin Mouriño <agus442m@gmail.com>
2022-08-25 15:02:39 -0300

    inital Commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# DEV-WP Plugin
-WordPress plugin for developers
+=== WordPress Dev Tools ===
+Donate link: agusdev.ar/donate
+Requires at least: 5.4.1
+Tested up to: 6.0.1
+Requires PHP: 7.8
+License: GPLv2 or later
+License URI: agusdev.ar/plugins
+
+WordPress plugin for developers.
+Contains various tools that help with general plugin and theme development
---
+++ dev-wp-plugin.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @package  dev-wp-plugin
+ */
+
+/*
+Plugin Name: WordPress Dev Tools
+Plugin URI: https://agusdev.ar/plugins/dev-wp-plugin/
+Description: WordPress plugin for developers. Contains various tools that help with general plugin and theme development
+Version: 1.0.0
+Author: Agustin Mouriño
+Author URI: https://agusdev.ar
+License: GPLv2 or later
+Text Domain: dev-wp-plugin
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+Copyright 2005-2015 Automattic, Inc.
+*/
+
+defined('ABSPATH') or die;
+
+if (!class_exists('DevWpPlugin')) {
+
+    class DevWpPlugin {
+    }
+}
